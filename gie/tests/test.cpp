7639dd81910eeb0feb802bb816c081c9e9dc5e58
alexge50 <alexge50.x@gmail.com>
2018-12-31 16:09:16 +0200

    added test

--- gie/tests/test.cpp
+++ gie/tests/test.cpp
@@ -6,7 +6,21 @@
 #include <gie/Graph.h>
 #include <gie/Program.h>
 
+#include <boost/python.hpp>
+
+#include <cassert>
+#include <vector>
+
 int main()
 {
+    Program program;
+
+    boost::python::object input{10};
+    NodeId castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"input", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
+
+    auto result = program.run();
+
+    assert(boost::python::extract<int>(input) == boost::python::extract<int>(result.value().m_object));
+
     return 0;
 }
