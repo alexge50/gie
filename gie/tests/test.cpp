e8099d7ac36417d208e8cb7f999f8f0f40b00e1f
alexge50 <alexge50.x@gmail.com>
2019-02-02 20:42:46 +0200

    corrected test

--- gie/tests/test.cpp
+++ gie/tests/test.cpp
@@ -14,32 +14,35 @@
 
 #include <iostream>
 
+
 TEST_CASE("GIE API tests", "[program]")
 {
-    Program program;
-    NodeId castToString, castToInt;
 
-    program.import("builtins");
-
-    boost::python::object input(10);
-
-    SECTION("single node")
+    SECTION("adding nodes")
     {
 
-        castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
+        Program program;
+        NodeId castToString, castToInt;
 
-        auto result = program.run();
+        program.import("builtins");
 
-        REQUIRE(std::to_string(boost::python::extract<int>(input)) == std::string{boost::python::extract<std::string>(result.value().m_object)});
+        boost::python::object input(10);
+
+        {
+            castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
+
+            auto result = program.run();
+
+            REQUIRE(std::to_string(boost::python::extract<int>(input)) == std::string{boost::python::extract<std::string>(result.value().m_object)});
+        }
+
+        {
+            castToInt = program.addNode(Node{{}, {"int", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"str"}}, std::variant<NodeId, Value>{castToString})}}});
+
+            auto result = program.run();
+
+            REQUIRE(boost::python::extract<int>(input) == boost::python::extract<int>(result.value().m_object));
+        }
     }
 
-    SECTION("two nodes")
-    {
-        castToInt = program.addNode(Node{{}, {"int", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"str"}}, std::variant<NodeId, Value>{castToString})}}});
-
-        auto result = program.run();
-
-        std::cout << boost::python::extract<int>(result.value().m_object);
-        REQUIRE(boost::python::extract<int>(input) == boost::python::extract<int>(result.value().m_object));
-    }
 }
