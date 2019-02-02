47458e6b9567186c8678f04719af5dd8fff07cb7
alexge50 <alexge50.x@gmail.com>
2019-02-02 20:43:47 +0200

    added dev test

---
+++ gie/tests/dev_test.cpp
@@ -0,0 +1,41 @@
+//
+// Created by alex on 2/2/19.
+//
+
+#include <gie/Node.h>
+#include <gie/Program.h>
+
+#include <boost/python.hpp>
+
+#include <cassert>
+#include <vector>
+
+#include <iostream>
+
+int main()
+{
+
+    Program program;
+    NodeId castToString, castToInt;
+
+    program.import("builtins");
+
+    boost::python::object input(10);
+
+    {
+        castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
+
+        auto result = program.run();
+
+        std::cout << (std::to_string(boost::python::extract<int>(input)) == std::string{boost::python::extract<std::string>(result.value().m_object)}) << std::endl;
+    }
+
+
+    {
+        castToInt = program.addNode(Node{{}, {"int", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"str"}}, std::variant<NodeId, Value>{castToString})}}});
+
+        auto result = program.run();
+
+        std::cout << (boost::python::extract<int>(input) == boost::python::extract<int>(result.value().m_object)) << std::endl;
+    }
+}
