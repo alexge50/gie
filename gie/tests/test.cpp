1f2cd5010a539822489b5f23bc66661a086ce61e
alexge50 <alexge50.x@gmail.com>
2019-02-13 20:18:28 +0200

    removing nodes test

--- gie/tests/test.cpp
+++ gie/tests/test.cpp
@@ -18,30 +18,36 @@
 TEST_CASE("GIE API tests", "[program]")
 {
 
-    SECTION("adding nodes")
+    Program program;
+    program.import("builtins");
+
+    boost::python::object input(10);
+
+    SECTION("a couple of nodes")
     {
-
-        Program program;
         NodeId castToString, castToInt;
-
-        program.import("builtins");
-
-        boost::python::object input(10);
-
+        castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
+        SECTION("1 node run")
         {
-            castToString = program.addNode(Node{{}, {"str", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"int"}}, std::variant<NodeId, Value>{Value{"int", input}})}}});
-
             auto result = program.run();
-
             REQUIRE(std::to_string(boost::python::extract<int>(input)) == std::string{boost::python::extract<std::string>(result.value().m_object)});
         }
 
+        castToInt = program.addNode(Node{{}, {"int", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"str"}}, std::variant<NodeId, Value>{castToString})}}});
+        SECTION("2 nodes run")
         {
-            castToInt = program.addNode(Node{{}, {"int", std::vector<std::pair<Argument, std::variant<NodeId, Value>>>{std::make_pair(Argument{"", {"str"}}, std::variant<NodeId, Value>{castToString})}}});
-
             auto result = program.run();
-
             REQUIRE(boost::python::extract<int>(input) == boost::python::extract<int>(result.value().m_object));
+        }
+
+        SECTION("removing node")
+        {
+            program.removeNode(castToInt);
+            auto result = program.run();
+            REQUIRE(std::to_string(boost::python::extract<int>(input)) == std::string{boost::python::extract<std::string>(result.value().m_object)});
+
+            program.removeNode(castToString);
+            auto result2 = program.run();
         }
     }
 
