f48c280f62cd326a0910eb4880d315acb4858c20
alexge50 <alexge50.x@gmail.com>
2019-05-19 01:11:35 +0300

    updated build things

--- gui/build.py
+++ gui/build.py
@@ -1,13 +1,20 @@
 import os
 
 install_path = '../dump'
-modules = ['nodeeditor']
+modules = ['nodeeditor', '../gie']
 
 os.system('mkdir build')
 os.chdir('build')
 for module in modules:
-    os.system(f'cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX:PATH={install_path}  ../../3rdparty/{module}')
-    os.system('cmake --build . --target install')
+    dir = module.split('/')[-1]
+    try:
+        os.mkdir(f'{dir}')
+    except:
+        pass
+    os.chdir(f'{dir}')
+    os.system(f'cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX:PATH=../{install_path}  ../../../3rdparty/{module}')
+    os.system('cmake --build . --target install -j4')
+    os.chdir('..')
+
 
 os.chdir('..')
-os.system('rm -rf build')
