164354e1bd7a5d6fa0df153b18e5480eeeed5cb0
alexge50 <alexge50.x@gmail.com>
2019-05-19 08:59:27 +0300

    added arithmetic module

---
+++ gui/modules/arithmetic.py
@@ -0,0 +1,11 @@
+def add(a: float, b: float) -> float:
+    return a + b
+
+def subtract(a: float, b: float) -> float:
+    return a - b
+
+def multiply(a: float, b: float) -> float:
+    return a * b
+
+def divide(a: float, b: float) -> float:
+    return a / b
