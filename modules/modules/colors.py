6a98751cfb4f78c563f6442e91d332b45c83763e
alexge50 <alexge50.x@gmail.com>
2019-05-23 12:16:17 +0000

    slight mistake

--- modules/modules/colors.py
+++ modules/modules/colors.py
@@ -1,16 +1,16 @@
 import modules.internals
 
-def to_color(r: double, g:double, b: double) -> modules.internals.Color:
-    return Color(r, g, b)
+def to_color(r: float, g: float, b: float) -> modules.internals.Color:
+    return modules.internals.Color(r, g, b)
 
 def invert(color: modules.internals.Color) -> modules.internals.Color:
-    return Color(1. - color.r, 1. - color.g, 1. - color.b)
+    return modules.internals.Color(1. - color.r, 1. - color.g, 1. - color.b)
 
-def red_channel(color: modules.internals.Color) -> double:
+def red_channel(color: modules.internals.Color) -> float:
     return color.r
 
-def green_channel(color: modules.internals.Color) -> double:
+def green_channel(color: modules.internals.Color) -> float:
     return color.g
 
-def blue_channel(color: modules.internals.Color) -> double:
+def blue_channel(color: modules.internals.Color) -> float:
     return color.b
