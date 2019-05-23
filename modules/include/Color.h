06c8032e3712887bb3b8a723c428c34bf6303dcf
alexge50 <alexge50.x@gmail.com>
2019-05-23 11:56:23 +0000

    added missing constructors

--- modules/include/Color.h
+++ modules/include/Color.h
@@ -7,7 +7,11 @@
 
 struct Color
 {
+    Color(): r{0}, g{0}, b{0} {}
     Color(double r_, double g_, double b_): r{r_}, g{g_}, b{b_} {}
+    Color(const Color&) = default;
+    Color(Color&&) = default;
+
     double r, g, b;
 };
 
