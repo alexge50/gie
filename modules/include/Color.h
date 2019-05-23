0b2fd4bcac3b6a2c57a11b7fbc9e42d924324ebf
alexge50 <alexge50.x@gmail.com>
2019-05-23 11:46:42 +0000

    separated modules

---
+++ modules/include/Color.h
@@ -0,0 +1,14 @@
+//
+// Created by alex on 5/23/19.
+//
+
+#ifndef MODULES_COLOR_H
+#define MODULES_COLOR_H
+
+struct Color
+{
+    Color(double r_, double g_, double b_): r{r_}, g{g_}, b{b_} {}
+    double r, g, b;
+};
+
+#endif //MODULES_COLOR_H
---
+++ modules/src/internals.cpp
@@ -0,0 +1,16 @@
+//
+// Created by alex on 5/23/19.
+//
+
+#include <boost/python.hpp>
+#include <Color.h>
+
+using namespace boost::python;
+
+BOOST_PYTHON_MODULE(internals)
+{
+    class_<Color>("Color", init<double, double, double>())
+            .def_readwrite("r", &Color::r)
+            .def_readwrite("g", &Color::r)
+            .def_readwrite("b", &Color::b);
+}
