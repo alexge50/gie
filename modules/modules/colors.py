9bc3ab1b9433f97c68bbca427fc3c1bc284639d9
alexge50 <alexge50.x@gmail.com>
2019-05-23 12:05:07 +0000

    added color <-> number handling functions

--- modules/modules/colors.py
+++ modules/modules/colors.py
@@ -1,4 +1,16 @@
 import modules.internals
 
+def to_color(r: double, g:double, b: double) -> modules.internals.Color:
+    return Color(r, g, b)
+
 def invert(color: modules.internals.Color) -> modules.internals.Color:
-    return Color(r=1. - color.r, g=1. - color.g, b=1. - color.b)
+    return Color(1. - color.r, 1. - color.g, 1. - color.b)
+
+def red_channel(color: modules.internals.Color) -> double:
+    return color.r
+
+def green_channel(color: modules.internals.Color) -> double:
+    return color.g
+
+def blue_channel(color: modules.internals.Color) -> double:
+    return color.b
