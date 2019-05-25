e72824ac2a26a4f8ad2d4f5ab28c3bed99217b01
alexge50 <alexge50.x@gmail.com>
2019-05-25 14:53:43 +0300

    added filepicker widget

---
+++ gui/src/filepicker/filepicker.cpp
@@ -0,0 +1,56 @@
+//
+// Created by alex on 5/25/19.
+//
+
+#include "filepicker.h"
+
+#include <QHBoxLayout>
+#include <QFileDialog>
+
+FilePicker::FilePicker(QString filter, QWidget *parent):
+        QWidget(parent),
+        m_filter{std::move(filter)}
+{
+    auto layout = new QHBoxLayout(this);
+
+    layout->addWidget(m_filenameEdit = new QLineEdit(), 3);
+    layout->addWidget(m_selectFile = new QPushButton("Open..."), 1);
+
+    connect(
+            m_filenameEdit, &QLineEdit::textEdited,
+            this, &FilePicker::filenameChanged
+    );
+
+    connect(
+            m_selectFile, &QPushButton::pressed,
+            this, &FilePicker::selectFile
+    );
+}
+
+void FilePicker::selectFile()
+{
+    m_filename = QFileDialog::getOpenFileName(
+            this,
+            tr("Open File"),
+            QDir::homePath(),
+            m_filter
+    );
+
+    m_filenameEdit->setText(m_filename);
+    Q_EMIT fileChanged(m_filename);
+}
+
+void FilePicker::filenameChanged()
+{
+    if(isValidFile(m_filenameEdit->text()))
+    {
+        m_filename = m_filenameEdit->text();
+        Q_EMIT fileChanged(m_filename);
+    }
+    else m_filenameEdit->setText(m_filename);
+}
+
+bool FilePicker::isValidFile(const QString&)
+{
+    return true;
+}
---
+++ gui/src/filepicker/filepicker.h
@@ -0,0 +1,36 @@
+//
+// Created by alex on 5/25/19.
+//
+
+#ifndef GUI_FILEPICKER_H
+#define GUI_FILEPICKER_H
+
+#include <QWidget>
+#include <QLineEdit>
+#include <QPushButton>
+
+class FilePicker : public QWidget
+{
+Q_OBJECT
+public:
+    explicit FilePicker(QString filter = "*", QWidget *parent = nullptr);
+
+Q_SIGNALS:
+    void fileChanged(QString name);
+
+private Q_SLOTS:
+    void selectFile();
+    void filenameChanged();
+
+private:
+    bool isValidFile(const QString&);
+
+private:
+    QLineEdit* m_filenameEdit;
+    QPushButton* m_selectFile;
+
+    const QString m_filter;
+    QString m_filename;
+};
+
+#endif //GUI_FILEPICKER_H
