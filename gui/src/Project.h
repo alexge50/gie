6f18c69100a4fb56706124f643b1e6a0d1af8392
alexge50 <alexge50.x@gmail.com>
2019-05-26 01:09:26 +0300

    created Project

---
+++ gui/src/Project.h
@@ -0,0 +1,59 @@
+//
+// Created by alex on 5/25/19.
+//
+
+#ifndef GUI_PROJECT_H
+#define GUI_PROJECT_H
+
+#include <QString>
+#include <QDir>
+#include <QUuid>
+#include <QImage>
+#include <QJsonObject>
+
+#include <nodes/FlowScene>
+
+#include <string>
+
+struct ProjectImage
+{
+    QUuid id;
+    QString name;
+    QImage image;
+};
+
+class Project
+{
+private:
+    Project(QtNodes::FlowScene& scene, QDir);
+    Project(QtNodes::FlowScene& scene, QDir, QJsonObject);
+
+public:
+    Project(const Project&) = delete;
+    Project(Project&&) = default;
+
+    void save();
+    void importImage(QString filename, QString name);
+
+private:
+    QJsonValue serialiseImages();
+    QJsonValue serialiseScripts();
+
+    void deserialiseImages(QJsonValue);
+    void deserialiseScripts(QJsonValue);
+
+private:
+    std::map<QUuid, ProjectImage> m_images;
+
+    QtNodes::FlowScene& m_scene;
+    QDir m_projectDirectory;
+    QString m_name;
+
+    friend Project newProject(QDir dir, QString name, QtNodes::FlowScene& scene);
+    friend Project loadProject(QString directory, QtNodes::FlowScene& scene);
+};
+
+Project newProject(QDir dir, QString name, QtNodes::FlowScene& scene);
+Project loadProject(QString directory, QtNodes::FlowScene& scene);
+
+#endif //GUI_PROJECT_H
