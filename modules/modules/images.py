b2c4d408a7f8078eec871cba52724d3703b5509c
alexge50 <alexge50.x@gmail.com>
2019-05-28 03:05:51 +0300

    added new function

--- modules/modules/images.py
+++ modules/modules/images.py
@@ -21,3 +21,13 @@ def average_color(image: modules.internals.Image)->modules.internals.Color:
 
 def sample(image: modules.internals.Image, row: int, column: int)->modules.internals.Color:
     return image.pixel_at(row, column)
+
+def seperate_blue_channel(image: modules.internals.Image)->modules.internals.Image:
+    new_image = modules.internals.Image(image.width(), image.height())
+
+    for row in range(image.height()):
+        for column in range(image.width()):
+            color = image.pixel_at(row, column)
+            new_image.set_pixel(row, column, modules.internals.Color(0, 0, color.b))
+
+    return new_image
