33e8f59bdde8e7fa461a4ea0c566f54e8a959e6b
alexge50 <alexge50.x@gmail.com>
2019-06-26 02:35:26 +0300

    using builtin Color and Image

--- modules/modules/effects.py
+++ modules/modules/effects.py
@@ -1,11 +1,10 @@
 import modules.internals
-import modules.images_internal
 
-def pixel_sort(source: modules.internals.Image, mask: modules.internals.Image)->modules.internals.Image:
+def pixel_sort(source: Image, mask: Image)->Image:
     return modules.images_internal.pixel_sort(source, mask)
 
-def pixel_distort_displace(source: modules.internals.Image, map: modules.internals.Image, row_factor: float, column_factor: float)->modules.internals.Image:
+def pixel_distort_displace(source: Image, map: Image, row_factor: float, column_factor: float)->Image:
     return modules.images_internal.pixel_distort_displace(source, map, row_factor, column_factor)
 
-def displacement(source: modules.internals.Image, map: modules.internals.Image, row_factor: float, column_factor: float)->modules.internals.Image:
+def displacement(source: Image, map: Image, row_factor: float, column_factor: float)->Image:
     return modules.images_internal.displacement(source, map, row_factor, column_factor)
