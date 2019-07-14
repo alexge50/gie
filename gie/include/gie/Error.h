ad4186671c38e14ffdbde8ff757844bab06e2494
alexge50 <alexge50.x@gmail.com>
2019-07-14 17:19:12 +0300

    added NodeInterfaceError

---
+++ gie/include/gie/Error.h
@@ -0,0 +1,31 @@
+//
+// Created by alex on 7/14/19.
+//
+
+#ifndef GIE_ERROR_H
+#define GIE_ERROR_H
+
+class NodeInterfaceError
+{
+public:
+    enum class errors
+    {
+        TypeCheckingFailed = 0,
+        IncorrectNodeId,
+        IncorrectSymbolName
+    };
+
+    explicit NodeInterfaceError(errors error): m_error{error} {}
+
+    const char* what()
+    {
+        static const char* names[] = {"TypeCheckingFailed", "IncorrectNodeId", "IncorrectSymbolName"};
+
+        return names[static_cast<int>(m_error)];
+    }
+
+private:
+    errors m_error;
+};
+
+#endif //GIE_ERROR_H
