fc99d00edcc85bc0a084a8b0503f75ef6778587f
alexge50 <alexge50.x@gmail.com>
2019-07-15 18:51:21 +0300

    modified TypeData (for QtNodes) to not contain any data

--- gui/src/gie/types/TypeData.h
+++ gui/src/gie/types/TypeData.h
@@ -5,13 +5,67 @@
 #ifndef GUI_TYPEDATA_H
 #define GUI_TYPEDATA_H
 
-#include <gie/Value.h>
+#include <gie/NodeId.h>
 #include <nodes/NodeData>
 
 class TypeData: public QtNodes::NodeData
 {
 public:
-    virtual Value value() = 0;
+    explicit TypeData(): m_nodeId{std::nullopt} {}
+    explicit TypeData(NodeId id): m_nodeId{id} {}
+
+    TypeData(const TypeData&) = default;
+    TypeData(TypeData&&) = default;
+
+    const auto& nodeId() { return m_nodeId; }
+
+private:
+    std::optional<NodeId> m_nodeId;
+};
+
+class StringData: public TypeData
+{
+public:
+    QtNodes::NodeDataType type() const override
+    {
+        return {"string", "string"};
+    }
+};
+
+class NumberData: public TypeData
+{
+public:
+    QtNodes::NodeDataType type() const override
+    {
+        return {"number", "number"};
+    }
+};
+
+class IntegerData: public TypeData
+{
+public:
+    QtNodes::NodeDataType type() const override
+    {
+        return {"integer", "integer"};
+    }
+};
+
+class ImageData: public TypeData
+{
+public:
+    QtNodes::NodeDataType type() const override
+    {
+        return {"image", "image"};
+    }
+};
+
+class ColorData: public TypeData
+{
+public:
+    QtNodes::NodeDataType type() const override
+    {
+        return {"color", "color"};
+    }
 };
 
 
