8df99bc5480286cc987cba28f023865664db9d03
alexge50 <alexge50.x@gmail.com>
2019-07-15 19:19:03 +0300

    removed python <-> gui type conversion

--- gui/src/gie/types/ExtractTypes.h
+++ gui/src/gie/types/ExtractTypes.h
@@ -9,65 +9,9 @@
 #include <gie/Argument.h>
 #include <boost/python.hpp>
 
-#include "StringData.h"
-#include "NumberData.h"
-#include "IntegerData.h"
-#include "ColorData.h"
-#include "ImageData.h"
-
 #include "TypeData.h"
 
 #include <memory>
-
-Value extractGieValue(const std::shared_ptr<QtNodes::NodeData>& nodeData)
-{
-    if(auto p = dynamic_cast<TypeData*>(nodeData.get()); p)
-    {
-        return p->value();
-    }
-
-    return Value{};
-}
-
-std::shared_ptr<QtNodes::NodeData> extractNodeData(const Value& value)
-{
-    if(auto x = boost::python::extract<long long>{value.m_object}; x.check() && PyLong_Check(value.m_object.ptr()))
-        return std::make_shared<IntegerData>(value);
-
-    if(auto x = boost::python::extract<double>{value.m_object}; x.check() && PyFloat_Check(value.m_object.ptr()))
-        return std::make_shared<NumberData>(value);
-
-    if(auto x = boost::python::extract<std::string>{value.m_object}; x.check())
-        return std::make_shared<StringData>(value);
-
-    if(auto x = boost::python::extract<Color>{value.m_object}; x.check())
-        return std::make_shared<ColorData>(value);
-
-    if(auto x = boost::python::extract<Image>{value.m_object}; x.check())
-        return std::make_shared<ImageData>(value);
-
-    return nullptr;
-}
-
-std::shared_ptr<QtNodes::NodeData> extractNodeData(const Type& type, const Value& value)
-{
-    if(type == Type("str"))
-        return std::make_shared<StringData>(value);
-
-    if(type == Type("float"))
-        return std::make_shared<NumberData>(value);
-
-    if(type == Type("int"))
-        return std::make_shared<IntegerData>(value);
-
-    if(type == Type("Color"))
-        return std::make_shared<ColorData>(value);
-
-    if(type == Type("Image"))
-        return std::make_shared<ImageData>(value);
-
-    return nullptr;
-}
 
 QtNodes::NodeDataType getTypeData(const Type& type)
 {
