621972cc55278cfbe6cdf876d6b193aef580c124
alexge50 <alexge50.x@gmail.com>
2019-07-17 00:09:44 +0300

    changed StringDisplayDataModel to work with the new interface

--- gui/src/gie/DisplayNodeDataModel/StringDisplayDataModel.cpp
+++ gui/src/gie/DisplayNodeDataModel/StringDisplayDataModel.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "StringDisplayDataModel.h"
-#include "src/gie/types/StringData.h"
+#include "src/gie/types/TypeData.h"
 
 StringDisplayDataModel::StringDisplayDataModel():
         m_label{new QLabel()}
@@ -11,34 +11,13 @@ StringDisplayDataModel::StringDisplayDataModel():
     m_label->setMargin(3);
 }
 
-unsigned int StringDisplayDataModel::nPorts(QtNodes::PortType portType) const
-{
-    if(portType == QtNodes::PortType::In)
-        return 1;
-    return 0;
-}
-
 QtNodes::NodeDataType StringDisplayDataModel::dataType(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const
 {
-    return StringData().type();
+    return StringTypeData().type();
 }
 
-void StringDisplayDataModel::setInData(std::shared_ptr<QtNodes::NodeData> input, QtNodes::PortIndex portIndex)
+void StringDisplayDataModel::displayData(Data data)
 {
-    if(auto* data = dynamic_cast<StringData*>(input.get()); data)
-    {
-        modelValidationState = QtNodes::NodeValidationState::Valid;
-        modelValidationError = QString();
-
-        m_label->setText(data->asText());
-    }
-    else
-    {
-        modelValidationState = QtNodes::NodeValidationState::Valid;
-        modelValidationError = QStringLiteral("Missing or incorrect inputs");
-
-        m_label->clear();
-    }
-
-    m_label->adjustSize();
+    const std::string& text = std::get<std::string>(data);
+    m_label->setText(QString::fromStdString(text));
 }
