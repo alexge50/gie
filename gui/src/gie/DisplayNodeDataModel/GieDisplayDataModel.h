d209a63d8d4e7cf82ff5afc3acf10ad01609779b
alexge50 <alexge50.x@gmail.com>
2019-07-17 00:06:52 +0300

    added GieDisplayDataModel

---
+++ gui/src/gie/DisplayNodeDataModel/GieDisplayDataModel.h
@@ -0,0 +1,54 @@
+//
+// Created by alex on 7/16/19.
+//
+
+#ifndef GIE_GIEDISPLAYDATAMODEL_H
+#define GIE_GIEDISPLAYDATAMODEL_H
+
+#include <QtCore/QObject>
+#include <nodes/NodeDataModel>
+#include <src/gie/types/TypeData.h>
+
+class GieDisplayDataModel: public QtNodes::NodeDataModel
+{
+    Q_OBJECT
+public:
+    std::shared_ptr<QtNodes::NodeData> outData(QtNodes::PortIndex portIndex) override { return {}; }
+
+    unsigned int nPorts(QtNodes::PortType portType) const override
+    {
+        if(portType == QtNodes::PortType::In)
+            return 1;
+        return 0;
+    }
+
+    void setInData(std::shared_ptr<QtNodes::NodeData> input, QtNodes::PortIndex portIndex) override
+    {
+        if(input)
+        {
+            modelValidationState = QtNodes::NodeValidationState::Valid;
+            modelValidationError = QString();
+
+            Q_EMIT dataUpdated(0);
+        }
+        else
+        {
+            modelValidationState = QtNodes::NodeValidationState::Valid;
+            modelValidationError = QStringLiteral("Missing or incorrect inputs");
+
+            Q_EMIT dataInvalidated(0);
+        }
+    }
+
+    QtNodes::NodeValidationState validationState() const override { return modelValidationState; }
+    QString validationMessage() const override { return modelValidationError; }
+
+public Q_SLOTS:
+    virtual void displayData(Data data) = 0;
+
+private:
+    QtNodes::NodeValidationState modelValidationState = QtNodes::NodeValidationState::Warning;
+    QString modelValidationError = QString("Missing or incorrect inputs");
+};
+
+#endif //GIE_GIEDISPLAYDATAMODEL_H
