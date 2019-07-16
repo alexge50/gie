7a4259c26685858a85f149e60dba14a92ba1a855
alexge50 <alexge50.x@gmail.com>
2019-07-17 00:09:19 +0300

    changed IntegerDisplayDataModel to work with the new interface

--- gui/src/gie/DisplayNodeDataModel/IntegerDisplayDataModel.cpp
+++ gui/src/gie/DisplayNodeDataModel/IntegerDisplayDataModel.cpp
@@ -3,7 +3,7 @@
 //
 
 #include "IntegerDisplayDataModel.h"
-#include "src/gie/types/IntegerData.h"
+#include "src/gie/types/TypeData.h"
 
 IntegerDisplayDataModel::IntegerDisplayDataModel():
         m_label{new QLabel()}
@@ -11,34 +11,14 @@ IntegerDisplayDataModel::IntegerDisplayDataModel():
     m_label->setMargin(3);
 }
 
-unsigned int IntegerDisplayDataModel::nPorts(QtNodes::PortType portType) const
-{
-    if(portType == QtNodes::PortType::In)
-        return 1;
-    return 0;
-}
-
 QtNodes::NodeDataType IntegerDisplayDataModel::dataType(QtNodes::PortType portType, QtNodes::PortIndex portIndex) const
 {
-    return IntegerData().type();
+    return IntegerTypeData().type();
 }
 
-void IntegerDisplayDataModel::setInData(std::shared_ptr<QtNodes::NodeData> input, QtNodes::PortIndex portIndex)
+void IntegerDisplayDataModel::displayData(Data data)
 {
-    if(auto* data = dynamic_cast<IntegerData*>(input.get()); data)
-    {
-        modelValidationState = QtNodes::NodeValidationState::Valid;
-        modelValidationError = QString();
+    long long int number = std::get<long long int>(data);
 
-        m_label->setText(data->asText());
-    }
-    else
-    {
-        modelValidationState = QtNodes::NodeValidationState::Valid;
-        modelValidationError = QStringLiteral("Missing or incorrect inputs");
-
-        m_label->clear();
-    }
-
-    m_label->adjustSize();
+    m_label->setText(QString::number(number));
 }
