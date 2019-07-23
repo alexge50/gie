5fc9c81d7d2e05c9f97a402eddb4bdc6427ca759
alexge50 <alexge50.x@gmail.com>
2019-07-24 00:28:52 +0300

    removed 'distributed noise'

--- modules/modules/noise.py
+++ modules/modules/noise.py
@@ -2,6 +2,3 @@ import modules.internals
 
 def perlin_noise(source: Image, seed: int, zoom: float, z: float, octaves: int, persistence: float)->Image:
     return modules.images_internal.perlin_noise(source, seed, zoom, z, octaves, persistence)
-
-def distributed_noise(source: Image) -> Image:
-    return to_image(np.random.rand(source.height(), source.width(), 3))
