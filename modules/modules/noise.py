f66927deb76ea1d23d6337f252dccdfd3fbb05d2
alexge50 <alexge50.x@gmail.com>
2019-07-23 18:05:08 +0300

    added distributed_noise

--- modules/modules/noise.py
+++ modules/modules/noise.py
@@ -2,3 +2,6 @@ import modules.internals
 
 def perlin_noise(source: Image, seed: int, z: float, octaves: int, persistence: float)->Image:
     return modules.images_internal.perlin_noise(source, seed, z, octaves, persistence)
+
+def distributed_noise(source: Image) -> Image:
+    return to_image(np.random.rand(source.height(), source.width(), 3))
