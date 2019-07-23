eb85b33d72bf83ea15a7efb8d53ced3cd16e091b
alexge50 <alexge50.x@gmail.com>
2019-07-23 18:04:46 +0300

    added sobel and sobel_2 in effects

--- modules/modules/effects.py
+++ modules/modules/effects.py
@@ -1,5 +1,9 @@
 import modules.internals
 
+import numpy as np
+import scipy.signal as signal
+import scipy.ndimage as ndimage
+
 def pixel_sort(source: Image, mask: Image)->Image:
     return modules.images_internal.pixel_sort(source, mask)
 
@@ -11,3 +15,37 @@ def displacement(source: Image, map: Image, row_factor: float, column_factor: fl
 
 def lens_distortion(source: Image, strength: float, zoom: float) -> Image:
     return modules.images_internal.lens_distortion(source, strength, zoom)
+
+def sobel(source: Image) -> Image:
+    a = to_ndarray(source)
+
+    channels = []
+    for d in range(3):
+        channels.append(ndimage.sobel(a[:, :, d]))
+
+    return to_image(np.stack(channels, axis=2))
+
+def sobel_2(source: Image) -> Image:
+    a = to_ndarray(source)
+
+    sobel_x = np.c_[
+        [-1, 0, 1],
+        [-2, 0, 2],
+        [-1, 0, 1]
+    ]
+
+    sobel_y = np.c_[
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ]
+
+    channels = []
+    for d in range(3):
+        sx = signal.convolve2d(a[:, :, d], sobel_x, mode='same',
+                               boundary='symm')
+        sy = signal.convolve2d(a[:, :, d], sobel_y, mode='same',
+                               boundary='symm')
+        channels.append(np.sqrt(sx ** 2 + sy ** 2))
+
+    return to_image(np.stack(channels, axis=2))
