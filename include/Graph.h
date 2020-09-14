acb84bc4868fa42fd530b585d4219ec11595e3a1
alexge50 <alexge50.x@gmail.com>
2020-09-14 19:59:40 +0100

    added Graph representation

---
+++ include/Graph.h
@@ -0,0 +1,36 @@
+#ifndef NODE_EDITOR_GRAPH_H
+#define NODE_EDITOR_GRAPH_H
+
+#include <vector>
+#include <string>
+#include <unordered_map>
+#include <glm/vec2.hpp>
+
+using NodeId = std::int32_t;
+using PortId = std::int32_t;
+
+struct Node
+{
+    std::vector<std::string> input_ports;
+    std::vector<std::string> output_ports;
+    std::string node_name;
+
+    glm::vec2 position;
+};
+
+struct Connection
+{
+    struct Port
+    {
+        NodeId node_id;
+        PortId port_id;
+    } output_port, input_port;
+};
+
+struct Graph
+{
+    std::unordered_map<NodeId, Node> nodes;
+    std::vector<Connection> connections;
+};
+
+#endif //NODE_EDITOR_GRAPH_H
